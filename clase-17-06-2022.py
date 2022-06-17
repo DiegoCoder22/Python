21b0909a2dd3448d98daf6b161a52fe09c597fb1
Diego Zepeda <diego.zepeda.b87@gmail.com>
2022-06-17 23:24:27 +0000

    respaldo

--- clase-17-06-2022.py
+++ clase-17-06-2022.py
@@ -1,1 +1,17 @@
-# Tipos de funciones
+# Funciones:#
+# Son aquellas que dividen el programa en partes, considerando la parte princila y los diferentes metodos (tareas) que deben realizar.
+# Las funciones deben ser embocadas.#
+# Tipos de funciones:#
+# 1) sin argumentos y sin retornos #
+# 2) sin argumentos y con retorno #
+# 3) con argumentos y sin retonro #
+# 4) con argumentos y con retonro #
+
+# 1) sin argumentos y sin retornos:
+def saludar(): # cramos la duncion
+    print('Hola chuchetumare') # los argumentos van dentro del parentesis.
+
+saludar() # aca estamos ejecutando la funcion
+
+# 2) sin argumentos y con retorno #
+def 

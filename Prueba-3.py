fe7ba4b05bb1e2e0c1faa882cc1587aa35213a52
Diego Zepeda <diego.zepeda.b87@gmail.com>
2022-07-02 01:09:13 +0000

    mejora

--- Prueba-3.py
+++ Prueba-3.py
@@ -1,50 +1,51 @@
 import os
-
 nif=list();niftipo=list();nombre=list();edad=list();reg=list()
 ciudadano=[nif, niftipo, nombre, edad, reg]
 
 while True:
-    print('Bienvenido al registro de ciudadanos de la Unión Europea de España, de Andalucía')
-    print('Seleccione una opción:')
-    print('1) Ingrese ciudadano')
-    print('2) Buscar ciudadano')
-    print('3) Imprimir certificado')
-    print('4) Salir')
+    def menu():
+        print('Bienvenido al registro de ciudadanos de la Unión Europea de España, de Andalucía')
+        print('Seleccione una opción:')
+        print('1) Ingrese ciudadano')
+        print('2) Buscar ciudadano')
+        print('3) Imprimir certificado')
+        print('4) Salir')
+    menu()
     op = input('Ingrese opción:')
     if op=='1':
-        while True:
+        while True: #Ciclo y validacion de nombre:
+            xnombre = input('Ingrese su Nombre:')
+            if len(xnombre)>=8:
+                input('(OK) Nombre ingresado es correcto')
+                break
+            else:
+                input('(x) Nombre ingresado es incorrecto... pulse una tecla...')
+            nombre.append(xnombre)
+        while True: #Ciclo y validacion de Edad:
             try:
-                xnombre = input('Ingrese su Nombre:')
-                if len(xnombre)>=8:
-                    input('Nombre ingresado es correcto')
-                else:
-                    input('Nombre ingresado es incorrecto')
-                break
                 xedad = int(input('Ingrese edad:'))
-                if xedad>=15 and xedd<=90:
+                if xedad>=15 and xedad<=90:
                     input('Edad ingresada es correcta')
-                else:
-                    input('Edad ingresada es incorrecta')
-                break
-                xnif = input('Ingrese NIF Ciudadano:')
-
-                xniftipo = input('Ingrese tipo -RTX, -XXY, -PEU, -03F:')
-                if len(xnif)==8:
-                    input(f'NIF ingresado {xnif} es correcto')
-                else:
-                    input('NIF no válido')
-                    input('Precione tencla para intentar nuevamente....')
                     break
-                    if ((xnif[0:3].isalpha() and xnif[2:2+4].isnumeric()) or (xnif[0:4].isalpha() and xnif[4:4+2].isnumeric())):
-                        if len(xnombre)!=8: #validacion, si el largo de xmar no es igual a 0.
-                            if len(xedad)!=0: #validacion si no es igual a 0.
-                                xniftipo = xniftipo.upper()
-                                if xniftipo=='-RTX' or xniftipo=='-XXY' or xniftipo=='-PEU' or xniftipo=='-03F':
-                                    input('datos ingresados correctamente')
-                                    break
             except Exception as e:
                 input('datos ingresados tienen error....',e)
-        nif.append(xnif);nombre.append(xnombre);edad.append(xedad);niftipo.append(xniftipo);reg.append('Reg:')
+            edad.append(xedad)
+        while True:
+            xnif = input('Ingrese NIF Ciudadano:')
+            if len(xnif)==8 or ((xnif[0:8].isalpha() and xnif[2:2+4].isnumeric()) or (xnif[0:4].isalpha() and xnif[4:4+2].isnumeric())):
+                input(f'NIF ingresado {xnif} es correcto')
+                break
+                nif.append(xnif)
+        while True:
+            xniftipo = input('Ingrese tipo -RTX, -XXY, -PEU, -03F:')
+            xniftipo = xniftipo.upper()
+            if xniftipo=='-RTX' or xniftipo=='-XXY' or xniftipo=='-PEU' or xniftipo=='-03F':
+                input(f'Tipo de NIF {xniftipo} ingresado es correcto.')
+                break
+            else:
+                input('NIF no válido, Precione tencla para intentar nuevamente....')
+            niftipo.append(xniftipo)
+            reg.append('reg:')
     elif op=='2':
         xnif=input('Ingrese NIF Ciudadano:')
         if xnif in nif:

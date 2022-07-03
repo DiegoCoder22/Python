71a2a32e784017a683381eec2a33602c6ede93cb
Diego Zepeda <diego.zepeda.b87@gmail.com>
2022-07-03 01:36:28 +0000

    subida buena

--- Prueba-3.py
+++ Prueba-3.py
@@ -1,13 +1,19 @@
 import os
 
-nif=list();niftipo=list();nombre=list();edad=list();reg=list()
-ciudadano=[nif, niftipo, nombre, edad, reg]
+#nif=list();niftipo=list();nombre=list();edad=list();reg=list()
+#ciudadano=[nif, niftipo, nombre, edad, reg]
+nif=[]
+niftipo=[]
+nombre=[]
+edad=[]
+reg=[nif, niftipo, nombre, edad]
 
 def salida():
     print("Gracias por utilizar el registro de ciudadanos de la Unión Europea de España, de Andalucía.")
 
 while True:
     def menu():
+        print('')
         print('Bienvenido al registro de ciudadanos de la Unión Europea de España, de Andalucía')
         print('Seleccione una opción:')
         print('1) Ingrese ciudadano')
@@ -21,42 +27,47 @@ while True:
             xnombre = input('Ingrese su Nombre:')
             if len(xnombre)>=8:
                 input('(OK) Nombre ingresado es correcto')
+                nombre.append(xnombre)
+                reg.append(reg)
                 break
             else:
                 input('(X) Nombre ingresado es incorrecto... pulse una tecla...')
-            nombre.append(xnombre)
         while True: #Ciclo y validacion de Edad:
             try:
                 xedad = int(input('Ingrese edad:'))
                 if xedad>=15 and xedad<=90:
                     input('(OK) Edad ingresada es correcta')
+                    edad.append(xedad)
+                    reg.append(reg)
                     break
             except Exception as e:
                 input('(X) datos ingresados tienen error....',e)
-            edad.append(xedad)
         while True: #Ciclo y validacion de NIF:
             xnif = input('Ingrese NIF Ciudadano:')
             if len(xnif)==8 or ((xnif[0:8].isalpha() and xnif[2:2+4].isnumeric()) or (xnif[0:4].isalpha() and xnif[4:4+2].isnumeric())):
                 input(f'(OK) NIF ingresado {xnif} es correcto')
                 nif.append(xnif)
+                reg.append(reg)
                 break   
         while True: #Ciclo y validacion de Tipo de NIF:
             xniftipo = input('Ingrese tipo -RTX, -XXY, -PEU, -03F:')
             xniftipo = xniftipo.upper()
             if xniftipo=='-RTX' or xniftipo=='-XXY' or xniftipo=='-PEU' or xniftipo=='-03F':
                 input(f'(OK) El de NIF: {xnif} y el NIF Tipo: {xniftipo} ingresados son correctos.')
+                niftipo.append(xniftipo)
+                reg.append(reg)
                 break
             else:
                 input('(X) NIF no válido, Precione tencla para intentar nuevamente....')
-            niftipo.append(xniftipo)
-            reg.append('reg:')
     elif op=='2': #Opcion 2
         cnif=input('Ingrese NIF del ciudadano: ')
         for i in range(len(nif)):
             if cnif in nif:
+                print('')
                 print('Se ha encontrado al Ciudadano consutado: ')
                 print('Datos del ciudadano: ')
                 print('NIF: ', nif[i])
+                print('NIF: ', niftipo[i])
                 print('Nombre: ', nombre[i])
                 print('Edad: ', edad[i])
                 input('Ingrese una tecla para continuar...')
